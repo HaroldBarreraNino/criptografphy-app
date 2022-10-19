aae634390a344fd36b674ab283d4dd339e924928
HaroldBarreraNino <107732633+HaroldBarreraNino@users.noreply.github.com>
2022-10-19 16:29:47 -0500

    Creating Components

---
+++ frontend/src/components/chatPage/chatPage.js
@@ -0,0 +1,9 @@
+const ChatPage = () => {
+    return(
+        <>
+        <h1>CHAT PAGE</h1>
+        </>
+    );
+}
+
+export default ChatPage;
---
+++ frontend/src/components/registerPage/registerPage.js
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+    return(
+        <>
+        <h1>REGISTER PAGE</h1>
+        </>
+    );
+}
+
+export default RegisterPage;
